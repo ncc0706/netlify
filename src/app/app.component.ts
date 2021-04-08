3e16635e79e6b2f810c6461ed13b5aeb01c8c16b
niuyuxian <ncc0706@gmail.com>
2021-04-08 14:51:55 +0800

    add hls support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
-import { setTheme } from 'ngx-bootstrap/utils';
+import DPlayer from 'dplayer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+
   title = 'netlify';
 
-  constructor(){
-    console.info("xxxxxxxx");
-    setTheme('bs3');
+  @ViewChild('dplayer') dplayer: ElementRef;
+
+  constructor() {
+
+  }
+
+
+  ngOnInit(): void {
+
+    // const that = this;
+    // window.onload = function() {
+    //   console.log(that.dplayer.nativeElement);
+    // };
+
+  }
+
+  ngAfterViewInit(): void {
+    const dp = new DPlayer({
+      container: this.dplayer.nativeElement,
+      video: {
+        url: 'https://cdn.jsdelivr.net/gh/cn-cdn/word-of-honor@latest/01/index.m3u8',
+        type: 'hls'
+      }
+    });
+
   }
 
 }
